10dbf1b47ba30fec66455770c35c133437ff3792
unknown <dnkuber2002@gmail.com>
2021-09-06 15:03:42 +0530

    hotel 2 commit

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const menuIcon = document.querySelector(".menuIcon");
-const navbar = document.querySelector(".navbar");
-let open = false;
+// const menuIcon = document.querySelector(".menuIcon");
+// const navbar = document.querySelector(".navbar");
+// let open = false;
 
-function handleMenuBar() {
-  if (open) {
-    menuIcon.style.backgroundImage = "url('/assets/hamburger.svg')";
-    open = false;
-    navbar.classList.remove("show");
+// function handleMenuBar() {
+//   if (open) {
+//     menuIcon.style.backgroundImage = "url('/assets/hamburger.svg')";
+//     open = false;
+//     navbar.classList.remove("show");
+//   } else {
+//     menuIcon.style.backgroundImage = "url('/assets/close.svg')";
+//     open = true;
+//     navbar.classList.add("show");
+//   }
+//   menuIcon.style.backgroundPosition = "center";
+//   menuIcon.style.backgroundSize = "center";
+//   menuIcon.style.backgroundRepeat = "no-repeat";
+// }
+
+const hamburger = document.querySelector(".hamburgerContainer");
+const navigation = document.querySelector(".navigation");
+let hamburgerOpen = false;
+
+hamburger.addEventListener("click", () => {
+  if (hamburgerOpen) {
+    hamburger.classList.remove("open");
+    navigation.style.left = "-100vw";
+    hamburgerOpen = false;
   } else {
-    menuIcon.style.backgroundImage = "url('/assets/close.svg')";
-    open = true;
-    navbar.classList.add("show");
+    hamburger.classList.add("open");
+    navigation.style.left = "0vw";
+    hamburgerOpen = true;
   }
-  menuIcon.style.backgroundPosition = "center";
-  menuIcon.style.backgroundSize = "center";
-  menuIcon.style.backgroundRepeat = "no-repeat";
-}
+});
 
 function removeShow() {
-  navbar.classList.remove("show");
-  open = false;
-  menuIcon.style.backgroundImage = "url('/assets/hamburger.svg')";
-  menuIcon.style.backgroundPosition = "center";
-  menuIcon.style.backgroundSize = "center";
-  menuIcon.style.backgroundRepeat = "no-repeat";
+  hamburger.classList.remove("open");
+  navigation.style.left = "-100vw";
+  hamburgerOpen = false;
 }
